502fe99a986ed9deb6814caeef020bf4609bc5f0
Patrick Schuur <patrickschuurdev@gmail.com>
2024-05-14 16:45:08 +0200

    Box2D working

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-﻿#include <glad/glad.h>
+﻿#include <box2d/box2d.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <imgui.h>
 
@@ -31,10 +32,53 @@ int main() {
   
   shader.Use();
   shader.SetMatrix4("projection", projection);
+  
+  b2Vec2 gravity{0.0f, -10.0f};
+  b2World world{gravity};
+  
+  b2BodyDef groundBodyDef;
+  groundBodyDef.position.Set(0.0f, -10.0f);
+  
+  b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+  b2PolygonShape groundBox;
+  groundBox.SetAsBox(50.0f, 10.0f);
+
+  groundBody->CreateFixture(&groundBox, 0.0f);
+
+  b2BodyDef bodyDef;
+  bodyDef.type = b2_dynamicBody;
+  bodyDef.position.Set(0.0f, 4.0f);
+  b2Body* body = world.CreateBody(&bodyDef);
+
+  b2PolygonShape dynamicBox;
+  dynamicBox.SetAsBox(1.0f, 1.0f);
+
+  b2FixtureDef fixtureDef;
+  fixtureDef.shape = &dynamicBox;
+  fixtureDef.density = 1.0f;
+  fixtureDef.friction = 0.3f;
+
+  body->CreateFixture(&fixtureDef);
+
+  float timeStep = 1.0f / 60.0f;
+
+  int32 velocityIterations = 6;
+  int32 positionIterations = 2;
+
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
-	
+
+    for (int32 i = 0; i < 60; ++i)
+    {
+	  world.Step(timeStep, velocityIterations, positionIterations);
+	  b2Vec2 position = body->GetPosition();
+	  float angle = body->GetAngle();
+	  printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+    }
+
+
     ClearScreen(clearColour);
     ImGuiManager::NewFrame();
 	
