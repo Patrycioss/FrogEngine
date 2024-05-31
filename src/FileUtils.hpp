26068095af23e511c720a6a031a6833eeb53f56f
Patrick Schuur <patrickschuurdev@gmail.com>
2024-05-31 10:28:48 +0200

    Added file utils

---
+++ src/FileUtils.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <fstream>
+
+namespace FileUtils{
+  
+  inline bool Exists(const char* _path){
+	if (FILE* file = fopen(_path, "r")){
+	  fclose(file);
+	  return true;
+	}
+	return false;
+  }
+}
